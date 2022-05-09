781859ddd1ffc2695ef20efb6e6ed54ad167dadb
IJH96 <97975318+IJH96@users.noreply.github.com>
2022-05-08 20:02:42 -0500

    Add files via upload
    This is working code of the small particles.

---
+++ GravPotSmall.py
@@ -0,0 +1,55 @@
+import math
+import numpy as np
+class particle:
+    def __init__  (self, position, mass):
+        self.position = position
+        self.m = mass
+       
+
+G = 6.67430E-11
+h= 1E-15
+def gravPotential(position, particles, h):
+    
+    phi = 0.0
+    for p in particles:
+        r = math.sqrt((position[0] - p.position[0])**2 + (position[1] - p.position[1])**2 + (position[2] - p.position[2])**2)
+        if (r>h/2):
+            
+            phi += (-G*p.m)/(r)
+    return phi
+
+particles = []
+
+f = open("particleSmall.txt","r")
+
+lines = f.readlines()
+for line in lines:
+    x,y,z , m  = line.split()
+    position = np.array([float(x), float(y), float(z) ])
+    p = particle(position, float(m))
+    particles.append(p)
+f.close()
+
+def centDiffGrav3D(f, position, h, n, particles):
+    
+
+    x_1 = position.copy()
+    x_1[n] += h/2
+    x_2 = position.copy()
+    x_2[n] -= h/2
+    
+    D_c = (f(x_1,particles,h) - f(x_2,particles,h))/h
+    return D_c
+
+
+x,y,z = input("Enter the location for acceleration value:").split()
+
+position = np.array([float(x),float(y),float(z)])
+
+
+a_x = -centDiffGrav3D(gravPotential, position, h,0,particles)
+a_y = -centDiffGrav3D(gravPotential, position,h,1, particles)
+a_z = -centDiffGrav3D(gravPotential, position,h,2, particles)
+    
+
+print(a_x, a_y, a_z)
